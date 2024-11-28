59b9f24490e5ac0d96872c68703a33e8caf170b7
trung <trung@nextgeneration.io>
2024-11-28 16:21:47 +0700

    completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/js/:path*", // Áp dụng cho tất cả file trong /js/
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS" },
+                ],
+            },
+        ];
+    },
+}
 
 module.exports = nextConfig
